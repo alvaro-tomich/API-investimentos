ecbb23d4106043267b1920f3b3e6061ed2a63fd5
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-18 12:50:20 -0300

    fix: consertando erro ao rodar o comando npx sequelize:create

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_HOST,
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,

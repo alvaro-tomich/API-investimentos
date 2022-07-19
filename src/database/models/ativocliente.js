adcb681899f672dea58f71c6ba3069af669c4f12
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 19:29:22 -0300

    fix: consertando associação e erros do lint

--- src/database/models/ativocliente.js
+++ src/database/models/ativocliente.js
@@ -2,23 +2,27 @@ const scriptClientSchema = (sequelize, dataTypes) => {
   const scriptClientTable = sequelize.define('AtivoCliente', {
     usuario: { type: dataTypes.INTEGER, primaryKey: true },
     ativo: { type: dataTypes.INTEGER, primaryKey: true },
-    qtdAtivo: sequelize.INTEGER,
-    total: sequelize.FLOAT
-  })
+    qtdAtivo: dataTypes.INTEGER,
+    total: dataTypes.FLOAT,
+  });
 
   scriptClientTable.associate = (models) => {
-    models.BlogPost.belongsToMany(models.Usuario, {
-        through: scriptClientTable,
-        foreignKey: 'ativo',
-        otherKey: 'usuario',
-        as: 'usuarios' 
+    models.AtivoCliente.belongsToMany(models.Usuario, {
+      through: scriptClientTable,
+      foreignKey: 'ativo',
+      otherKey: 'usuario',
+      as: 'usuarios',
     });
 
-    models.Category.belongsToMany(models.Ativo, {
-        through: scriptClientTable,
-        foreignKey: 'usuario',
-        otherKey: 'ativo',
-        as: 'ativos' 
+    models.AtivoCliente.belongsToMany(models.Ativo, {
+      through: scriptClientTable,
+      foreignKey: 'usuario',
+      otherKey: 'ativo',
+      as: 'ativos',
     });
-  }
-}
+  };
+
+  return scriptClientTable;
+};
+
+module.exports = scriptClientSchema;

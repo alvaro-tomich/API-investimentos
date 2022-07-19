a114485f4bd5c2160c36004307c1d38c8f4b8b1b
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 14:51:29 -0300

    feat: criado model conta e feita associação com a model usuario

---
+++ src/database/models/conta.js
@@ -0,0 +1,16 @@
+const walletSchema = (sequelize, dataTypes) => {
+  const walletTable = sequelize.define('Conta', {
+    codConta: { allowNull: false, type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    saldo: dataTypes.DECIMAL,
+  }, { timestamps: false, underscored: true })
+
+  walletTable.associate = (models) => {
+    walletTable.belongsTo(models.Usuario, {
+      foreignKey: 'usuario', as: 'usuarios'
+    })
+  }
+
+  return walletTable
+}
+
+module.exports = walletSchema;

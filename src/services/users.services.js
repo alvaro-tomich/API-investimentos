6925e2d66d856ee491b970425552a52bba158a5d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 20:40:16 -0300

    feat(services/users): criada função para criar conta do usuario

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,7 +1,13 @@
 const { Usuario } = require('../database/models');
+const { Conta } = require('../database/models');
+
+const createWallet = async (saldo, usuario) => {
+  await Conta.create({ saldo, usuario });
+};
 
 const createUser = async ({ nome, senha, email }) => {
   const newUser = await Usuario.create({ nome, senha, email });
+  await createWallet(0, newUser.codUsuario);
 
   return newUser.codUsuario;
 };

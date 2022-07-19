27e73a38dd1daef802cda0b82cf950e71af0daf5
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 13:07:23 -0300

    feat: criado model de endereco

---
+++ src/database/models/endereco.js
@@ -0,0 +1,14 @@
+const addressSchema = (sequelize, dataTypes) => {
+  const addressTable = sequelize.define('Endereco', {
+    codEndereco: { allowNull: false, type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    rua: dataTypes.STRING,
+    numero: dataTypes.INTEGER,
+    bairro: dataTypes.STRING,
+    cidade: dataTypes.STRING,
+    estado: dataTypes.STRING,
+  }, { timestamps: false, underscored: true })
+
+  return addressTable;
+};
+
+module.exports = addressSchema;

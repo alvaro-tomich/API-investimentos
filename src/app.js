1d55d1d6b69e79d4542c86bc949f21c205699f02
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 19:51:13 -0300

    feat(app): fazendo com que a aplicação utilize as rotas de user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const userRouter = require('./routes/user.route');
+
 const app = express();
 
 app.use(express.json());
+app.use(userRouter);
 
 module.exports = app;

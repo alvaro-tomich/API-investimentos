b2115b147a15764759ce8e56978d4b9753469b1d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 19:29:04 -0300

    fix: consertando associação e erros do lint

--- src/database/models/ativo.js
+++ src/database/models/ativo.js
@@ -1,24 +1,26 @@
 const scripSchema = (sequelize, dataTypes) => {
   const scripTable = sequelize.define('Ativo', {
-    codAtivo: { allowNull: false, type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    codAtivo: {
+      allowNull: false, type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true,
+    },
     nomeAtivo: dataTypes.STRING(10),
-    qtdAtivo: dataTypes.INT,
-    valorAtivo: dataTypes.DECIMAL
-  }, { timestamps: false, underscored: true })
+    qtdAtivo: dataTypes.INTEGER,
+    valorAtivo: dataTypes.DECIMAL,
+  }, { timestamps: false, underscored: true });
 
   scripTable.associate = (models) => {
     scripTable.hasMany(models.Venda, {
-      foreignKey: ativo, as: 'venda'
-    })
-  }
+      foreignKey: 'ativo', as: 'venda',
+    });
+  };
 
   scripTable.associate = (models) => {
     scripTable.hasMany(models.Compra, {
-      foreignKey: ativo, as: 'compras'
-    })
-  }
+      foreignKey: 'ativo', as: 'compras',
+    });
+  };
 
   return scripTable;
-}
+};
 
 module.exports = scripSchema;

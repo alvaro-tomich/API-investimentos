3d1faaf82ca73cb7b1cf9d02a03f12f63f4edff0
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 14:51:50 -0300

    feat: criado migration de conta

---
+++ src/database/migrations/20220719173251-create-conta.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Conta', {
+      cod_conta: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      saldo: {
+        type: Sequelize.DECIMAL
+      },
+      usuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Usuarios',
+          key: 'cod_usuario',
+        },
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Conta');
+  }
+};

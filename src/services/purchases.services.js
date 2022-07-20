b06aa13a314126094bf6f4ed1066f70800b4defd
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 11:44:43 -0300

    feat(services/purchases): criado função que atualiza as quantidades de ativos

--- src/services/purchases.services.js
+++ src/services/purchases.services.js
@@ -1,7 +1,13 @@
-const { Compra } = require('../database/models');
+const { Compra, Ativo } = require('../database/models');
+
+const updateScripQuantity = async (codAtivo, qtdAtivo) => {
+  const ativo = await Ativo.findByPk(codAtivo);
+  await Ativo.update({ qtdAtivo: ativo.qtdAtivo - qtdAtivo }, { where: { codAtivo } });
+};
 
 const purchaseScrip = async ({ codCliente, codAtivo, qtdAtivo }) => {
   await Compra.create({ usuario: codCliente, ativo: codAtivo, qtdAtivo });
+  await updateScripQuantity(codAtivo, qtdAtivo);
 };
 
 module.exports = { purchaseScrip };

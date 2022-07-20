6971230da15d27b052dfbf784f70caa5af0e5c0d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 12:15:06 -0300

    feat(services/purchases): criado função que mescla cliente e ativo comprado

--- src/services/purchases.services.js
+++ src/services/purchases.services.js
@@ -1,13 +1,22 @@
-const { Compra, Ativo } = require('../database/models');
+const { Compra, Ativo, AtivoCliente } = require('../database/models');
 
 const updateScripQuantity = async (codAtivo, qtdAtivo) => {
   const ativo = await Ativo.findByPk(codAtivo);
   await Ativo.update({ qtdAtivo: ativo.qtdAtivo - qtdAtivo }, { where: { codAtivo } });
 };
 
+const mesclateScripAndClient = async (codCliente, codAtivo, qtdAtivo) => {
+  const ativo = await Ativo.findByPk(codAtivo);
+  const total = ativo.valorAtivo * qtdAtivo;
+  await AtivoCliente.create({
+    usuario: codCliente, ativo: codAtivo, qtdAtivo, total,
+  });
+};
+
 const purchaseScrip = async ({ codCliente, codAtivo, qtdAtivo }) => {
   await Compra.create({ usuario: codCliente, ativo: codAtivo, qtdAtivo });
   await updateScripQuantity(codAtivo, qtdAtivo);
+  await mesclateScripAndClient(codCliente, codAtivo, qtdAtivo);
 };
 
 module.exports = { purchaseScrip };

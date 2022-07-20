817a8b9b42c42c4705dec2ae33b9c3199106125c
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 13:38:52 -0300

    feat(controllers/purchases): retorna erro 422 caso não tenha saldo

--- src/controllers/purchases.controllers.js
+++ src/controllers/purchases.controllers.js
@@ -1,8 +1,10 @@
 const { purchaseScrip } = require('../services/purchases.services');
 
 const purchase = async (req, res) => {
-  await purchaseScrip(req.body);
-
+  const isBalanceOk = await purchaseScrip(req.body);
+  if (!isBalanceOk) {
+    return res.status(422).json({ message: 'Saldo insuficiente!' });
+  }
   return res.status(201).json({ message: 'Compra efetuada com sucesso!' });
 };
 

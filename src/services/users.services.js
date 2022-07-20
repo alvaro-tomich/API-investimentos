d85fd8fa269eed439a85a0e804fe06fecc94fa7d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-19 21:34:26 -0300

    feat(services/users): alterando retorno da função createUser para o token

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,6 +1,7 @@
 const { Usuario } = require('../database/models');
 const { Conta } = require('../database/models');
 const { Endereco } = require('../database/models');
+const { generateJWTToken } = require('../utils/JWTGenerate');
 
 const createWallet = async (saldo, usuario) => {
   await Conta.create({ saldo, usuario });
@@ -18,8 +19,9 @@ const createUser = async ({
   const newUser = await Usuario.create({ nome, senha, email });
   await createWallet(0, newUser.codUsuario);
   await createAddress(rua, numero, bairro, cidade, estado, newUser.codUsuario);
+  const token = generateJWTToken(JSON.stringify(newUser));
 
-  return newUser.codUsuario;
+  return token;
 };
 
 const deleteUser = async (codUsuario) => {

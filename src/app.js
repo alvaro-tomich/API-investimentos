f99973f2ebc19176402702ca941ee19406e10148
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 11:31:06 -0300

    feat: fazendo com que a aplicação enxergue as rotas de compras

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const userRouter = require('./routes/user.route');
+const purchasesRouter = require('./routes/purchases.route');
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(purchasesRouter);
 
 module.exports = app;

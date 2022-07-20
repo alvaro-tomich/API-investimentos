7edc69481acb750d76a2dd90a9ad39355ecadfc3
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 12:33:43 -0300

    feat(services/purchases): criado função que atualiza o saldo da conta

--- src/services/purchases.services.js
+++ src/services/purchases.services.js
@@ -1,13 +1,21 @@
-const { Compra, Ativo, AtivoCliente } = require('../database/models');
+const {
+  Compra, Ativo, AtivoCliente, Conta,
+} = require('../database/models');
 
 const updateScripQuantity = async (codAtivo, qtdAtivo) => {
   const ativo = await Ativo.findByPk(codAtivo);
   await Ativo.update({ qtdAtivo: ativo.qtdAtivo - qtdAtivo }, { where: { codAtivo } });
 };
 
+const updateBalance = async (codCliente, valor) => {
+  const conta = await Conta.findByPk(codCliente);
+  await Conta.update({ saldo: conta.saldo - valor }, { where: { usuario: codCliente } });
+};
+
 const mesclateScripAndClient = async (codCliente, codAtivo, qtdAtivo) => {
   const ativo = await Ativo.findByPk(codAtivo);
   const total = ativo.valorAtivo * qtdAtivo;
+  await updateBalance(codCliente, total);
   await AtivoCliente.create({
     usuario: codCliente, ativo: codAtivo, qtdAtivo, total,
   });

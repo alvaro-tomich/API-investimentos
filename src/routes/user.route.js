4ec3b86b13ea4a9c08f18fdf1ca0f431ff84771e
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 10:32:46 -0300

    feat(routers/users): add rota para buscar conta pelo cod do cliente

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  create, remove, deposit, withdraw,
+  create, remove, deposit, withdraw, getAccount,
 } = require('../controllers/users.controllers');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/user', create);
 router.post('/conta/deposito', deposit);
 router.post('/conta/saque', withdraw);
+router.get('/conta/:codCliente', getAccount);
 router.delete('/user/:id', remove);
 
 module.exports = router;

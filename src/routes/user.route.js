dd098578f1e3a3db4d2f22347242a23b8eb38c2b
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 20:52:06 -0300

    feat(routes): validação nas rotas POST /login, POST /conta/deposito e saque

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,13 +6,15 @@ const {
 const router = express.Router();
 const validateToken = require('../middlewares/validateToken');
 const userSchema = require('../schemas/user.schema');
+const loginSchema = require('../schemas/login.schema');
+const DWSchema = require('../schemas/deposit.withdraw.schema');
 const validateBody = require('../middlewares/validateBody');
 
 router.post('/user', validateBody(userSchema), create);
-router.post('/conta/deposito', validateToken, deposit);
-router.post('/conta/saque', validateToken, withdraw);
+router.post('/conta/deposito', validateToken, validateBody(DWSchema), deposit);
+router.post('/conta/saque', validateToken, validateBody(DWSchema), withdraw);
 router.get('/conta/:codCliente', validateToken, getAccount);
-router.post('/login', login);
+router.post('/login', validateBody(loginSchema), login);
 router.delete('/user/:id', validateToken, remove);
 
 module.exports = router;

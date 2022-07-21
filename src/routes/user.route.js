bb117ea6ea8cf011bcb1bf3c5ec769ab199359cc
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 12:05:04 -0300

    feat(routes/users): add rota de login

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  create, remove, deposit, withdraw, getAccount,
+  create, remove, deposit, withdraw, getAccount, login,
 } = require('../controllers/users.controllers');
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.post('/user', create);
 router.post('/conta/deposito', deposit);
 router.post('/conta/saque', withdraw);
 router.get('/conta/:codCliente', getAccount);
+router.post('/login', login);
 router.delete('/user/:id', remove);
 
 module.exports = router;

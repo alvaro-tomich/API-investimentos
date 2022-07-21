1b201dccb7a38b300d89cdd043498c0aaea84319
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 21:54:25 -0300

    feat(services/sales): adicionado função que verifica quantidade de venda

--- src/services/sales.services.js
+++ src/services/sales.services.js
@@ -1,7 +1,18 @@
-const { Venda } = require('../database/models');
+const { Venda, AtivoCliente } = require('../database/models');
+
+const verifySellQuantity = async (codCliente, codAtivo, qtdAtivo) => {
+  const [ativoCliente] = await AtivoCliente
+    .findAll({ where: { usuario: codCliente, ativo: codAtivo } });
+  if (ativoCliente.qtdAtivo < qtdAtivo) return false;
+  return true;
+};
 
 const createSale = async ({ codCliente, codAtivo, qtdAtivo }) => {
+  const isSeelOk = await verifySellQuantity(codCliente, codAtivo, qtdAtivo);
+  if (!isSeelOk) return false;
   await Venda.create({ usuario: codCliente, ativo: codAtivo, qtdAtivo });
+
+  return true;
 };
 
 module.exports = { createSale };

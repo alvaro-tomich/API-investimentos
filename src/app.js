4bd032c40eb62d65b703cb7cdf4623e0dc807202
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 21:35:24 -0300

    feat: fazendo a api enxergar as rotas de vendas

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const userRouter = require('./routes/user.route');
 const purchasesRouter = require('./routes/purchases.route');
+const salesRouter = require('./routes/sales.route');
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
 app.use(purchasesRouter);
+app.use(salesRouter);
 
 module.exports = app;

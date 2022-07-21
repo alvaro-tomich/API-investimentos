38125bcc4077a381dbc64f5850517d579bc2a1df
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 08:58:16 -0300

    feat(routes/investimentos): add rota get ativos/:codAtivo

--- src/routes/investiments.route.js
+++ src/routes/investiments.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { getScripClient } = require('../controllers/investiments.controllers');
+const { getScripClient, getScrip } = require('../controllers/investiments.controllers');
 
 router.get('/investimentos/:codCliente', getScripClient);
+router.get('/ativos/:codAtivo', getScrip);
 
 module.exports = router;

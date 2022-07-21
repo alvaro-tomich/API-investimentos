bd9334e6f2a266aec277a1deeb704dd61cb8041e
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 08:50:10 -0300

    feat: fazendo o app enxergar as rotas de investimentos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const userRouter = require('./routes/user.route');
 const purchasesRouter = require('./routes/purchases.route');
 const salesRouter = require('./routes/sales.route');
+const investimentsRouter = require('./routes/investiments.route');
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(purchasesRouter);
 app.use(salesRouter);
+app.use(investimentsRouter);
 
 module.exports = app;

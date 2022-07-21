d6610ed56f7af43525995ad9a975246ab4c0f881
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 09:32:18 -0300

    feat(routes/users): criado rota para deposito

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { create, remove } = require('../controllers/users.controllers');
+const { create, remove, deposit } = require('../controllers/users.controllers');
 
 const router = express.Router();
 
 router.post('/user', create);
+router.post('/conta/deposito', deposit);
 router.delete('/user/:id', remove);
 
 module.exports = router;

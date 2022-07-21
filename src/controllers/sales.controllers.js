31b85a689ca234021b3cfa20d461084fb474e456
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-20 21:55:29 -0300

    feat(controllers/sales): condição que retorna erro caso quantidade de venda for insuficiente

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -1,8 +1,11 @@
 const { createSale } = require('../services/sales.services');
 
 const create = async (req, res) => {
-  await createSale(req.body);
-  res.status(201).json({ message: 'Venda realizada com sucesso!' });
+  const isSaleOk = await createSale(req.body);
+  if (!isSaleOk) {
+    return res.status(422).json({ message: 'Quantidade insuficiente!' });
+  }
+  return res.status(201).json({ message: 'Venda realizada com sucesso!' });
 };
 
 module.exports = { create };

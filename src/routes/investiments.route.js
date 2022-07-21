50e06881ea6604d9af1ae9616df8d70ed613c725
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 11:16:25 -0300

    feat(routes/investments): add rota que retorna todos ativos

--- src/routes/investiments.route.js
+++ src/routes/investiments.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const { getScripClient, getScrip } = require('../controllers/investiments.controllers');
+const { getScripClient, getScrip, getScrips } = require('../controllers/investiments.controllers');
 
 router.get('/investimentos/:codCliente', getScripClient);
+router.get('/ativos', getScrips);
 router.get('/ativos/:codAtivo', getScrip);
 
 module.exports = router;

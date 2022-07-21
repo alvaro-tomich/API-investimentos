8c9448c6c9caebcb065206056558e319578c2daf
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 09:41:20 -0300

    feat(routes/users): add rota para realizar deposito

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { create, remove, deposit } = require('../controllers/users.controllers');
+const {
+  create, remove, deposit, withdraw,
+} = require('../controllers/users.controllers');
 
 const router = express.Router();
 
 router.post('/user', create);
 router.post('/conta/deposito', deposit);
+router.post('/conta/saque', withdraw);
 router.delete('/user/:id', remove);
 
 module.exports = router;

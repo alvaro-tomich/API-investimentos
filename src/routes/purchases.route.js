2f9958b26341ca6ce6ba2406644419c3f2f7e107
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-21 21:05:10 -0300

    feat(routes) validações nas rotas POST /investimentos/comprar e vender

--- src/routes/purchases.route.js
+++ src/routes/purchases.route.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const { purchase } = require('../controllers/purchases.controllers');
 const validateToken = require('../middlewares/validateToken');
+const validateBody = require('../middlewares/validateBody');
+const transactionsSchema = require('../schemas/transactions.schema');
 
-router.post('/investimentos/comprar', validateToken, purchase);
+router.post('/investimentos/comprar', validateToken, validateBody(transactionsSchema), purchase);
 
 module.exports = router;
--- src/routes/sales.route.js
+++ src/routes/sales.route.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const { create } = require('../controllers/sales.controllers');
 const validateToken = require('../middlewares/validateToken');
+const validateBody = require('../middlewares/validateBody');
+const transactionsSchema = require('../schemas/transactions.schema');
 
-router.post('/investimentos/vender', validateToken, create);
+router.post('/investimentos/vender', validateBody(transactionsSchema), validateToken, create);
 
 module.exports = router;

bbc5d26079c7127f2e3657fbf67bfef29d0bbe7e
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-22 11:36:51 -0300

    feat(controllers/users): blocos try e catch e verificação de usuarios

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,48 +1,107 @@
+/* eslint-disable no-console */
 const {
-  createUser, deleteUser, depositServices, withdrawServices, getAccountByCod, loginService,
+  createUser,
+  deleteUser,
+  depositServices,
+  withdrawServices,
+  getAccountByCod,
+  loginService,
+  verifyUser,
+  getUser,
 } = require('../services/users.services');
 
 const create = async (req, res) => {
-  const token = await createUser(req.body);
+  try {
+    const token = await createUser(req.body);
 
-  res.status(201).json({ token });
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 const deposit = async (req, res) => {
-  const newDeposit = await depositServices(req.body);
-  if (!newDeposit) {
-    return res.status(422).json({ message: 'Quantidade a ser depositada deve ser maior que zero!' });
+  const { codCliente } = req.body;
+  try {
+    const { error, message } = await getUser(codCliente);
+    if (error && message) {
+      return res.status(error).json({ message });
+    }
+    const newDeposit = await depositServices(req.body);
+    if (!newDeposit) {
+      return res.status(422).json({ message: 'Quantidade a ser depositada deve ser maior que zero!' });
+    }
+
+    return res.status(201).json({ message: 'Depósito realizado com sucesso' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
   }
-  return res.status(201).json({ message: 'Depósito realizado com sucesso' });
 };
 
 const withdraw = async (req, res) => {
-  const newWithdraw = await withdrawServices(req.body);
-  if (!newWithdraw) {
-    return res.status(422).json({ message: 'Valor deve ser maior que zero e menor que o saldo atual!' });
+  const { codCliente } = req.body;
+  try {
+    const { error, message } = await getUser(codCliente);
+    if (error && message) {
+      return res.status(error).json({ message });
+    }
+    const newWithdraw = await withdrawServices(req.body);
+    if (!newWithdraw) {
+      return res.status(422).json({ message: 'Valor deve ser maior que zero e menor que o saldo atual!' });
+    }
+
+    return res.status(201).json({ message: 'Saque realizado com sucesso' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
   }
-  return res.status(201).json({ message: 'Saque realizado com sucesso' });
 };
 
 const getAccount = async (req, res) => {
-  const [account] = await getAccountByCod(req.params);
-  if (!account) {
-    return res.status(404).json({ message: 'Conta não encontrada' });
+  try {
+    const [account] = await getAccountByCod(req.params);
+    if (!account) {
+      return res.status(404).json({ message: 'Conta não encontrada' });
+    }
+
+    return res.status(200).json(account);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
   }
-  return res.status(200).json(account);
 };
 
 const remove = async (req, res) => {
   const codUsuario = req.params.id;
-  await deleteUser(codUsuario);
+  try {
+    const { error, message } = await getUser(codUsuario);
+    if (error && message) {
+      return res.status(error).json({ message });
+    }
+    await deleteUser(codUsuario);
 
-  return res.status(204).end();
+    return res.status(204).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 const login = async (req, res) => {
-  const token = await loginService(req.body);
+  try {
+    const { error, message } = await verifyUser(req.body);
+    if (error && message) {
+      return res.status(error).json({ message });
+    }
+    const token = await loginService(req.body);
 
-  return res.status(200).json({ token });
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 module.exports = {

046981cbe90f492f833ef6671d2ca5a06339179b
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-22 10:23:40 -0300

    feat(services/controllers): bloco try e catch e funções no controller

--- src/controllers/purchases.controllers.js
+++ src/controllers/purchases.controllers.js
@@ -1,11 +1,25 @@
-const { purchaseScrip } = require('../services/purchases.services');
+/* eslint-disable no-console */
+const {
+  purchaseScrip, updateScripQuantityAndTotal, mesclateScripAndClient, verifyScripAndClient,
+} = require('../services/purchases.services');
 
 const purchase = async (req, res) => {
-  const isBalanceOk = await purchaseScrip(req.body);
-  if (!isBalanceOk) {
-    return res.status(422).json({ message: 'Saldo insuficiente!' });
+  try {
+    const existScripAndClient = await verifyScripAndClient(req.body);
+    if (existScripAndClient.error) {
+      return res.status(existScripAndClient.error).json({ message: existScripAndClient.message });
+    }
+    const isBalanceOk = await purchaseScrip(req.body);
+    if (!isBalanceOk) {
+      return res.status(422).json({ message: 'Saldo insuficiente!' });
+    }
+    await updateScripQuantityAndTotal(req.body);
+    await mesclateScripAndClient(req.body);
+    return res.status(201).json({ message: 'Compra efetuada com sucesso!' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
   }
-  return res.status(201).json({ message: 'Compra efetuada com sucesso!' });
 };
 
 module.exports = { purchase };

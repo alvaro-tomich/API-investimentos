2737929beff85e6184e88bbed8005712bf6830a8
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-22 10:26:04 -0300

    feat(controllers/sales): bloco try e catch

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-console */
 const { createSale } = require('../services/sales.services');
 
 const create = async (req, res) => {
-  const isSaleOk = await createSale(req.body);
-  if (!isSaleOk) {
-    return res.status(422).json({ message: 'Quantidade insuficiente!' });
+  try {
+    const isSaleOk = await createSale(req.body);
+    if (!isSaleOk) {
+      return res.status(422).json({ message: 'Quantidade insuficiente!' });
+    }
+    return res.status(201).json({ message: 'Venda realizada com sucesso!' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
   }
-  return res.status(201).json({ message: 'Venda realizada com sucesso!' });
 };
 
 module.exports = { create };

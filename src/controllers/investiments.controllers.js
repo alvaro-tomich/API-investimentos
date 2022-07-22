e948ce1fe86b6627cf0d0e4bc885fc6f6c19aba3
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-22 10:48:02 -0300

    feat(controllers/investiments): add blocos try e catch)

--- src/controllers/investiments.controllers.js
+++ src/controllers/investiments.controllers.js
@@ -1,23 +1,36 @@
+/* eslint-disable no-console */
 const { getByClient, getScripService, getScripsService } = require('../services/investments.services');
 
 const getScripClient = async (req, res) => {
   const { codCliente } = req.params;
-  const ativosClientes = await getByClient(codCliente);
-
-  return res.status(200).json(ativosClientes);
+  try {
+    const ativosClientes = await getByClient(codCliente);
+    return res.status(200).json(ativosClientes);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 const getScrip = async (req, res) => {
   const { codAtivo } = req.params;
-  const ativo = await getScripService(codAtivo);
-
-  return res.status(200).json(ativo);
+  try {
+    const ativo = await getScripService(codAtivo);
+    return res.status(200).json(ativo);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 const getScrips = async (_req, res) => {
-  const scrips = await getScripsService();
-
-  return res.status(200).json(scrips);
+  try {
+    const scrips = await getScripsService();
+    return res.status(200).json(scrips);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Erro interno!' });
+  }
 };
 
 module.exports = { getScripClient, getScrip, getScrips };

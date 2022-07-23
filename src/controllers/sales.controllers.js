d7b1e47dfce7dacbb4ab9e3d2960811034cb5a1d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-07-23 14:50:49 -0300

    fix(controllers/sales): mensagem de erro alterada

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -11,7 +11,7 @@ const create = async (req, res) => {
     }
     const isSaleOk = await createSale(req.body);
     if (!isSaleOk) {
-      return res.status(422).json({ message: 'Quantidade insuficiente!' });
+      return res.status(422).json({ message: 'Quantidade de ativos insuficiente!' });
     }
     await updateScripQuantityAndValue(req.body);
     await updateScriptClient(req.body);
